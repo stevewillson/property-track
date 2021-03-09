8ef5f3a217bd2daaccb81793422d0e33992b9355
Stephen Willson <stephen.l.willson.mil@mail.mil>
2021-03-09 06:40:30 -0500

    change to async/await, add phr parsing with getannotations

--- src/components/ProcessFile/ProcessFile.js
+++ src/components/ProcessFile/ProcessFile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as pdfjsLib from 'pdfjs-dist/webpack';
 import { processShr } from './helperFunctions';
+import { processPhr } from './helperFunctions';
 import XLSX from 'xlsx';
 
 const ProcessFile = () => {
@@ -9,11 +10,24 @@ const ProcessFile = () => {
     try {
       const importFile = document.getElementById("importDataFile").files[0];
       const fileContents = await readFile(importFile);
-      parsePDF(fileContents).then(pdfData => {
-        let jsonData = processShr(pdfData);
-        exportToXlsx(jsonData["items"], 'property_track.xlsx');
-      });
-      
+
+      // try to read the contents of the file
+      const pdfData = await parsePDF(fileContents);
+
+      let jsonData = '';
+      let textContent = pdfData.textContent;
+      let annotText = pdfData.annotContent;
+      // check to see if the parsed PDF text includes 'Sub Hand Receipt'
+      if (textContent.includes('Sub Hand Receipt')){
+        jsonData = processShr(textContent);
+        exportToXlsx(jsonData.items, 'property_track.xlsx');
+      // check to see if the parsed PDF text includes 'Primary Hand Receipt'
+      } else if (textContent.includes('Primary Hand Receipt')) {
+          // also get the annotations for the pdf
+
+          jsonData = processPhr(textContent, annotText);
+          exportToXlsx(jsonData.items, 'property_track.xlsx');
+      }      
     } catch (e) {
       console.log(e.message);
     }
@@ -34,46 +48,59 @@ const ProcessFile = () => {
     });
   };
 
-  const parsePDF = (inputData) => {
+  const parsePDF = async (inputData) => {
+    // first, try to extract annotations from the PDF, then extract the text content
     //pdfjs.GlobalWorkerOptions.workerSrc = 'pdf.worker.js';
-    let loadingTask = pdfjsLib.getDocument(inputData);
-    return loadingTask.promise.then(function(pdf) {
-      // use *pdf* here
-      var maxPages = pdf._pdfInfo.numPages;
-      var pageTextPromises = []; // collecting all page promises
-      for (var j = 1; j <= maxPages; j++) {
-        var page = pdf.getPage(j);
-        pageTextPromises.push(page.then(function(page) { // add page promise
-          var textContent = page.getTextContent();
-          return textContent.then(function(page){ // return content promise
-            return page // value for page text
-          });
-        }));
-      }
-      // Wait for all pages and sum counts
-      
-      return Promise.all(pageTextPromises).then(function (pageText) {
-        var allPageTextItems = [];
-        pageText.forEach(function aggregatePages(pageTextCont) {
-          pageTextCont.items.forEach(function aggregatePageText(pageTextItems) {
-            allPageTextItems.push(pageTextItems);
-          });
-        });
-
-        // remove double quotes and whitespace before and after text
-        var pageTextTrim = allPageTextItems.map(propItem => propItem.str.replace(/("|,)/g,'').trim());
-
-        // remove blank lines from page text
-        var pageTextNoBlanks = pageTextTrim.filter(propItem => (propItem !== ""));
-        return pageTextNoBlanks;
-      });
+    let loadingTask = await pdfjsLib.getDocument({
+      data: inputData
     });
-  }
+    const results = await loadingTask.promise;
+    // use *pdf* here
+    var maxPages = results._pdfInfo.numPages;
+    var allPageTextItems = [];
+    var allPageAnnotItems = [];
+      
+    for (var j = 1; j <= maxPages; j++) {
+      var page = await results.getPage(j);
+      const textContent = await page.getTextContent();
+      const annotContent = await page.getAnnotations();
+      allPageTextItems.push(...textContent.items);
+      allPageAnnotItems.push(...annotContent);
+    }
+
+    // remove double quotes and whitespace before and after text
+    var pageTextTrim = allPageTextItems.map(propItem => propItem.str.replace(/("|,)/g,'').trim());
+
+    // remove blank lines from page text
+    var pageTextNoBlanks = pageTextTrim.filter(propItem => (propItem !== ""));
+
+    // remove double quotes and whitespace before and after text
+    var valsOfInterest = allPageAnnotItems.map(el => { 
+      return {
+        "alternativeText": el.alternativeText,
+        "fieldName": el.fieldName,
+        "fieldValue": el.fieldValue
+      };
+    });
+    var noNullVals = valsOfInterest.filter(el => el.fieldValue != null);
+    var pageAnnotTrim = noNullVals.map(item => {
+      return {
+        "fieldValue": item.fieldValue.replace(/("|,)/g,'').trim(),
+        "fieldName": item.fieldName,
+        "alternativeText": item.alternativeText,
+      };
+    });
+
+    // remove blank lines from page text
+    var pageAnnotNoBlanks = pageAnnotTrim.filter(propItem => (propItem.fieldValue !== ""));
+
+    return {"textContent": pageTextNoBlanks, "annotContent": pageAnnotNoBlanks};
+  };
 
   const exportToXlsx = (content, fileName) => {
     /* add to workbook */
     let wb = XLSX.utils.book_new();      
-    let ws = XLSX.utils.json_to_sheet(content)
+    let ws = XLSX.utils.json_to_sheet(content);
     XLSX.utils.book_append_sheet(wb, ws, 'Property Tracker');
 
     /* write workbook */
